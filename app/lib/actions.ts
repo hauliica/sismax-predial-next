c34d524a51a6b8f54c83a8f0aff0ae5ece7b93eb
Saul Ochoa <hauliica@gmail.com>
2023-11-29 15:13:46 -0600

    Change method case in actions.ts for compatibility
    Updated the casing of the database method in actions.ts. Changed 'prisma.padron' to 'prisma.Padron' to align with method naming conventions and avoid possible execution errors.

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -30,7 +30,7 @@ export async function verificaCuentaFolio(formData: FormData) {
   console.log(cuenta, folio);
 
   // Check if exists
-  const predio = await prisma.Padron.count({
+  const predio = await prisma.padron.count({
     where: {
       AND: [
         {
@@ -57,7 +57,7 @@ export async function verificaCuentaFolio(formData: FormData) {
 export async function fetchPredio(cuentaFolio: string) {
   const [cuenta, folio] = [cuentaFolio.slice(0, 6), cuentaFolio.slice(6, 12)];
 
-  const predio = await prisma.Padron.findFirst({
+  const predio = await prisma.padron.findFirst({
     where: {
       AND: [
         {
